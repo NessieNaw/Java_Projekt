fbe5dde45916e7cb74560f4418440cc9101f2c2e
Agnieszka <inessa.naw@gmail.com>
2019-05-11 23:51:08 +0200

    basically working, but image view does not

--- src/main/java/com/javafx/mavenproject/Panel.java
+++ src/main/java/com/javafx/mavenproject/Panel.java
@@ -1,5 +1,30 @@
 package com.javafx.mavenproject;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+
 public class Panel
 {
+    @FXML private ImageView imageView;
+
+    public void ShowImage(File obraz )
+    {
+        Image image = new Image(obraz.toURI().toString());
+        imageView = new ImageView(image);
+        System.out.println(obraz.getPath());
+
+    }
+
+
 }
--- src/main/java/com/javafx/mavenproject/StartWindow.java
+++ src/main/java/com/javafx/mavenproject/StartWindow.java
@@ -3,51 +3,80 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 
-public class StartWindow
+
+public class StartWindow extends Panel
 {
-    private static final Logger log = LoggerFactory.getLogger(ExampleController.class);
     @FXML private javafx.scene.control.Button closeButton;
     @FXML private javafx.scene.control.Button binarny;
     @FXML private javafx.scene.control.Button szary;
     @FXML private javafx.scene.control.Button kolorowy;
 
+    int which = 0;
+    File obraz = null;
 
-    public int chooseFile() throws Exception {
+    public int chooseFile() throws Exception
+    {
+        System.out.println(which);
         Stage stage = (Stage) binarny.getScene().getWindow();
-        System.out.println(stage);
-        Choose(stage);
-        openPanel(stage);
-
+        int c = Choose(stage);
+        if( c != 0 )
+            openPanel(stage);
 
         return 0;
     }
+    public int bin() throws Exception {
+        which = 1;
+        chooseFile();
+        return which;
+    }
+    public int sz() throws Exception {
+        which = 2;
+        chooseFile();
+        return which;
+    }
+    public int kol() throws Exception {
+        which = 3;
+        chooseFile();
+        return which;
+    }
     public void openPanel(Stage stage) throws Exception
     {
+        Panel pan = new Panel();
+        pan.ShowImage(obraz);
+
         String fxmlFile = "/fxml/panel.fxml";
         FXMLLoader loader = new FXMLLoader();
         Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
         Scene scene = new Scene(rootNode, 700,500);
         scene.getStylesheets().add("/styles/style2.css");
 
+
         stage.setTitle("Panel window");
         stage.setScene(scene);
         stage.show();
+
     }
-    public void Choose(Stage stage)
+    public int Choose(Stage stage)
     {
         FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Open Resource File");
-        fileChooser.showOpenDialog(stage);
+        fileChooser.setTitle("Open File");
 
+        File selectedFile = fileChooser.showOpenDialog(stage);
+        if (selectedFile != null)
+        {
+            //stage.display(selectedFile);
+            obraz = selectedFile;
+            System.out.println(obraz.getPath());
+            return 1;
+        }
+        return 0;
     }
     public void Close()
     {

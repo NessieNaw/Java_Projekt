7215177ed63ffc927c6c65ee628e59459abf001b
Agnieszka <inessa.naw@gmail.com>
2019-05-12 16:34:05 +0200

    commit

--- src/main/java/com/javafx/mavenproject/Panel.java
+++ src/main/java/com/javafx/mavenproject/Panel.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
@@ -14,15 +16,47 @@ import javafx.scene.image.ImageView;
 
 import java.io.File;
 
+
 public class Panel
 {
+
     @FXML private ImageView imageView;
+    @FXML private javafx.scene.control.Button otwarcie;
+    @FXML private javafx.scene.control.Button zamkniecie;
+    @FXML private javafx.scene.control.Button monochromatyzacja;
+    @FXML private javafx.scene.control.Button erozja;
+    @FXML private javafx.scene.control.Button dylacja;
+    @FXML private javafx.scene.control.Button normalizacja;
+    @FXML private javafx.scene.control.Button vmf;
+    @FXML private javafx.scene.control.Button progowanie;
+    @FXML private javafx.scene.control.Button kirch;
+    @FXML private javafx.scene.control.Button zamknieciesymetryczne;
+    @FXML private javafx.scene.control.Button odlegloscgeodezyjna;
+    @FXML private javafx.scene.control.Button binaryzacja;
+    @FXML private TextField progbinaryzacji;
+    @FXML private TextField lamanaA;
+    @FXML private TextField lamanaB;
+    @FXML private TextField lamanaC;
+    @FXML private TextField oknovmf;
+    @FXML private TextField promien;
 
     public void ShowImage(File obraz )
     {
         Image image = new Image(obraz.toURI().toString());
         imageView = new ImageView(image);
         System.out.println(obraz.getPath());
+
+    }
+    public void params()
+    {
+        String progBin = progbinaryzacji.getText();
+        String lamA = lamanaA.getText();
+        String lamB = lamanaB.getText();
+        String lamC = lamanaC.getText();
+        String oknoVMF = oknovmf.getText();
+        String pr = promien.getText();
+
+        System.out.println(lamA+ lamB+ lamC+ oknoVMF+ progBin+ pr );
 
     }
 

388fed82dcf56c3143899b1078ba21dcb3a3e074
MKT9 <kubit.magdalena@gmail.com>
2019-05-13 00:25:36 +0200

    To Binary update

--- src/main/java/com/javafx/mavenproject/morfologicalTransfLibrary/Monochrome.java
+++ src/main/java/com/javafx/mavenproject/morfologicalTransfLibrary/Monochrome.java
@@ -46,50 +46,29 @@ public class Monochrome
 
             }
         return binaryImage;
-
     }
 
     public BufferedImage ToBinary( BufferedImage bufferedImage)
     {
-        int width = bufferedImage.getWidth();
-        int height = bufferedImage.getHeight();
+        BufferedImage binaryImage = new BufferedImage(bufferedImage.getWidth(), bufferedImage.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
 
-        System.out.println( width + "width" + height+"height");
-        BufferedImage colorImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        BufferedImage binaryImage = new BufferedImage(colorImage.getWidth(), colorImage.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
-
-
+        int[][] array2D = ImageUtils.convertTo2DArray(bufferedImage);
         
-        
-        for (int x = 0; x < colorImage.getWidth(); x++)
-            for (int y = 0; y < colorImage.getHeight(); y++)
+        for (int x = 0; x < bufferedImage.getWidth(); x++)
+            for (int y = 0; y < bufferedImage.getHeight(); y++)
             {
-                Color mycolour = new Color(colorImage.getRGB(x, y));
-                int color = colorImage.getRGB(x,y);
-                int blue = color & 0xff;
-                int green = (color & 0xff00) >> 8;
-                int red = (color & 0xff0000) >> 16;
-                int alpha = (color & 0xff000000) >>> 24;
+                int pixel = array2D[y][x];
 
-                System.out.println(blue +" "+green+" "+red+" "+alpha+" ");
-                System.out.println(mycolour);
-                System.out.println(colorImage.getRaster().getSample(x, y, 0));  //R
-                System.out.println(colorImage.getRaster().getSample(x, y, 1));  //G
-                System.out.println(colorImage.getRaster().getSample(x, y, 2));  //B
-
-
-                
-
-                System.out.println(Integer.toString(colorImage.getRGB(x, y), 16));
-                if (rgbToGray(colorImage.getRGB(x, y)) > 127 )
+                if ((pixel & 0xff) > 127 )
                 {
-                    binaryImage.setRGB(x, y, 0);
+                    binaryImage.setRGB(x, y, 0xffffff);
                 }
                 else
-                    binaryImage.setRGB(x, y, 0xffffff);
+                    binaryImage.setRGB(x, y, 0);
             }
         return binaryImage;
     }
+
     private static int rgbToGray(int rgb)
     {
         int r = (rgb >> 16) & 0xff;
